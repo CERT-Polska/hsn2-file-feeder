d0397f96ef87906294633188b0144d07ce16b8d2
rafalkor <rafal.kordalski@nask.pl>
2013-02-07 17:46:58 +0100

    jsvc support added.closes #1

--- src/main/java/pl/nask/hsn2/service/FileFeederService.java
+++ src/main/java/pl/nask/hsn2/service/FileFeederService.java
@@ -19,22 +19,90 @@
 
 package pl.nask.hsn2.service;
 
+import java.lang.Thread.UncaughtExceptionHandler;
+
+import org.apache.commons.daemon.Daemon;
+import org.apache.commons.daemon.DaemonContext;
+import org.apache.commons.daemon.DaemonController;
+import org.apache.commons.daemon.DaemonInitException;
+
 import pl.nask.hsn2.CommandLineParams;
 import pl.nask.hsn2.GenericService;
 
-public final class FileFeederService {
-	private FileFeederService() {
+public final class FileFeederService implements Daemon{
+	private Thread serviceRunner = null;
+	private CommandLineParams cmd = null;
+
+	public static void main(final String[] args) throws DaemonInitException, Exception {
+		FileFeederService ffs = new FileFeederService();
+		ffs.init(new DaemonContext() {
+			
+			@Override
+			public DaemonController getController() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public String[] getArguments() {
+				return args;
+			}
+		});
+		ffs.start();
+		ffs.serviceRunner.join();
+		ffs.stop();
+		ffs.destroy();
 	}
 
-	public static void main(String[] args) throws InterruptedException {
-		CommandLineParams cmd = new CommandLineParams();
+	@Override
+	public void init(DaemonContext context) throws DaemonInitException,
+			Exception {
+		cmd = new CommandLineParams();
 		cmd.useDataStoreAddressOption(false);
 		cmd.setDefaultServiceNameAndQueueName("feeder-list");
 
-		cmd.parseParams(args);
-
-		GenericService service = new GenericService(new FileFeederTaskFactory(), cmd.getMaxThreads(), cmd.getRbtCommonExchangeName());
+		cmd.parseParams(context.getArguments());
+		final GenericService service = new GenericService(new FileFeederTaskFactory(), cmd.getMaxThreads(), cmd.getRbtCommonExchangeName());
 		cmd.applyArguments(service);
-		service.run();
+		
+		serviceRunner = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler() {			
+					@Override
+					public void uncaughtException(Thread t, Throwable e) {
+						System.exit(1);	
+					}
+				});
+				try {
+					service.run();
+				} catch (InterruptedException e1) {
+					System.exit(1);
+				}
+				
+			}
+		},"file-feeder-Service");
+		
+	}
+
+	@Override
+	public void start() throws Exception {
+		serviceRunner.start();
+	}
+
+	@Override
+	public void stop() throws Exception {
+		if ( serviceRunner == null)
+			return;
+		serviceRunner.interrupt();
+		serviceRunner.join(10000);
+		
+	}
+
+	@Override
+	public void destroy() {
+		serviceRunner = null;
+		
 	}
 }
